c60bd1590a252f24347637eeee0810789f689c7f
mafuyuk <mafuyuk@mafuyuk.local>
2017-03-11 18:26:53 +0900

    add: karabiner.json

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.nvimrc ~/.nvimrc
+ln -sf ~/dotfiles/.nvimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
