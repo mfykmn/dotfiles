ceacadf90b64bd2d3d7b0f45995ac550408ec18f
mafuyuk <mafuyuk.m@gmail.com>
2018-01-07 15:49:31 +0900

    Fixed path

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,7 +4,7 @@
 mkdir -p ~/.config/nvim/
 ln -sf ~/dotfiles/vim/.nvimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/vim/dein.toml ~/.config/nvim/dein.toml
-ln -sf ~/dotfiles/vim/dein.toml ~/.config/nvim/dein_lasy.toml
+ln -sf ~/dotfiles/vim/dein_lasy.toml ~/.config/nvim/dein_lasy.toml
 
 ### zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
