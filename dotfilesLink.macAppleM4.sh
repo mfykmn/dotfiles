409de9b9477cfd2e2e4876efca9cec3ef2519e5b
mfykmn <mfykmn@gmail.com>
2025-01-13 17:57:18 +0900

    update

--- dotfilesLink.macAppleM4.sh
+++ dotfilesLink.macAppleM4.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ### zsh
-ln -nfs ~/workspace/dotfiles/zsh/mac/apple-m2/.zshrc ~/.zshrc
-ln -nfs ~/workspace/dotfiles/zsh/mac/apple-m2/.zsh.d ~/.zsh.d
+ln -nfs ~/workspace/dotfiles/zsh/mac/apple-m4/.zshrc ~/.zshrc
+ln -nfs ~/workspace/dotfiles/zsh/mac/apple-m4/.zsh.d ~/.zsh.d
 
 ### Git
 ln -nfs ~/workspace/dotfiles/git/.gitconfig ~/.gitconfig
